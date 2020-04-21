182b1a635e6b4518bf44b447a31759038ef378d8
trafalgarDWaterLaw <mohitparitosh@gmail.com>
2020-04-21 18:24:52 +0100

    server changes

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ var path = require('path');
 
 
 app.use(express.static('public'))
-
+app.get('*', function(req, res) {
+    res.sendfile('./public/index.html');
+});
 app.use('/static', express.static('public'))
-app.use('/static', express.static(path.join(__dirname, 'public'))) 
+app.use('/static', express.static(path.join(__dirname, 'public')))
 app.listen(8080);
